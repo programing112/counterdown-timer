46ac7abea8344dbde7786ea07da433c1ee8c3291
Rana muhammad dawood <153193759+programing112@users.noreply.github.com>
2024-05-11 01:45:49 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+import { differenceInSeconds } from "date-fns";
+function* countdownTimer(second) {
+    while (second >= 0) {
+        yield second;
+        second--;
+    }
+}
+let timerIterator = countdownTimer(10);
+//function to creat a count down timer 
+function displayCountdown() {
+    let result = timerIterator.next();
+    if (!result.done) {
+        const now = new Date();
+        const countdownTime = new Date(now.getTime() + (result.value * 1000));
+        const remainingSeconds = differenceInSeconds(countdownTime, now);
+        console.log(`Remaining seconds: ${remainingSeconds}`);
+        setTimeout(displayCountdown, 1000);
+    }
+    else {
+        console.log("countdown complete!");
+    }
+}
+displayCountdown();
